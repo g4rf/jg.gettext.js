db1b9d9e566b4031cc747a8c9134408cc30bd486
g4rf <admin@g4rf.net>
2017-03-01 14:00:42 +0100

    inital commit...

---
+++ public_html/js/jqgettext-parser.js
@@ -0,0 +1,19 @@
+/**
+ * jqGettext is able to translate static HTML pages with the help of jQuery as
+ * GNU gettext does it with dynamic pages.
+ * 
+ * This file parses HTML and JS files with the help of jQuery und produces a
+ * JSON output. Save that output in a file, translate it and save it somewhere
+ * else with a correct ISO language code. This file is than be used by
+ * jqGettext to translate the HTML page on the fly.
+ * 
+ * @license GNU GPL v3 https://www.gnu.org/licenses/gpl-3.0
+ * @version 0.1alpha
+ * @author Jan Kossick admin@g4rf.net
+ * @updated 2017-03-01
+ * 
+ * TODO:
+ *  - add pot/mo/po support
+ */
+
+
